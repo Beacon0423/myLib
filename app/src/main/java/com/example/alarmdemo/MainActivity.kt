b915138236649dd31867704d342d9aa197c1f3ce
May <2096832525@qq.com>
2022-07-16 23:32:42 +0800

    220716.23.31

--- app/src/main/java/com/example/alarmdemo/MainActivity.kt
+++ app/src/main/java/com/example/alarmdemo/MainActivity.kt
@@ -7,26 +7,38 @@ import android.content.Intent
 import android.content.IntentFilter
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.alarmdemo.databinding.ActivityMainBinding
 import com.example.alarmdemo.receiver.AlarmReceiver
 import com.example.alarmdemo.service.AlarmService
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     @SuppressLint("ShortAlarm")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        //注册NotificationChannel，Android8之后需要，可根据自己选择
         val channel = NotificationChannel("xxm", "通知", NotificationManager.IMPORTANCE_HIGH)
         val manager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
         manager.createNotificationChannel(channel)
-
+        //动态注册BroadcastReceiver
         val receiver = AlarmReceiver()
         val filter = IntentFilter()
         filter.addAction("toAlarmReceiver")
         registerReceiver(receiver, filter)
+        //点击函数
+        click()
+    }
 
-        val intent = Intent(this, AlarmService::class.java)
-        intent.putExtra("time", 5)
-        startService(intent)
+    private fun click(){
+        val input = binding.edt.text.toString().trim()
+        if (input.isNotEmpty()) {
+            val intent = Intent(this, AlarmService::class.java)
+            intent.putExtra("time", input.toInt())
+            startService(intent)
+        }
     }
 }
