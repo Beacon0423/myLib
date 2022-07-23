310de6899ab9e396e99ac928c8b816f533c4d047
beacon0423 <2096832525@qq.com>
2022-07-23 11:37:43 +0800

    220723.11.37

--- mylibrary/src/main/java/com/example/mylibrary/Print.java
+++ mylibrary/src/main/java/com/example/mylibrary/Print.java
@@ -2,7 +2,7 @@ package com.example.mylibrary;
 
 import android.content.Context;
 
-public class Toast {
+public class Print {
     public static void pt(Context context, String msg){
         android.widget.Toast.makeText(context, msg, android.widget.Toast.LENGTH_SHORT).show();
     }
